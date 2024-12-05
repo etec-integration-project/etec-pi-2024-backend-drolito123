aa7c4442290d4bdb46196babb64a1cfb60b97744
drolito123 <p.pensado@alumno.etec.um.edu.ar>
2024-12-05 17:21:57 -0300

    .

--- src/route/Route.js
+++ src/route/Route.js
@@ -1,7 +1,6 @@
 import { Router } from 'express';
 import { getShirts, addShirt } from '../controller/shirtController.js';
 import { registerUser, loginUser } from '../controller/userController.js';
-import { getCart, addToCart, updateCartItem, clearCart } from '../controller/cartController.js';
 
 const router = Router();
 
@@ -13,10 +12,5 @@ router.post('/shirts', addShirt);
 router.post('/register', registerUser);
 router.post('/login', loginUser);
 
-// Rutas para el carrito
-router.get('/cart', getCart);
-router.post('/cart/add', addToCart);
-router.put('/cart/update/:id', updateCartItem);
-router.delete('/cart/clear', clearCart);
 
 export default router;
