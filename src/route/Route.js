8630ae1f8afb442fc38d27fe624dfc70564db6bc
drolito123 <p.pensado@alumno.etec.um.edu.ar>
2024-12-13 15:17:54 -0300

    .

--- src/route/Route.js
+++ src/route/Route.js
@@ -1,3 +1,4 @@
+import { request, response } from 'express';
 import { Router } from 'express';
 import { getShirts, addShirt, getShirtsId } from '../controller/shirtController.js';
 import { registerUser, loginUser, logout } from '../controller/userController.js';
@@ -6,9 +7,14 @@ import { buyCart } from '../controller/cartController.js';
 const router = Router();
 
 
+router.route('/shirts/:id', getShirtsId).get((request, response) => {
+    Db.getOrder(request.params.id).then((data) => {
+      response.json(data[0]);
+    })
+  })
+
 // Rutas para shirts
 router.get('/shirts', getShirts);
-router.get('/shirts/:name', getShirtsId);
 router.post('/shirts', addShirt);
 
 // Rutas para users
